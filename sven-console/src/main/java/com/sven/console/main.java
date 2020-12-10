466c6bde26eabd8bdc8abdcaa855f96afd13c6d5
jian.tang <Welcome@2019>
2020-12-10 10:06:18 +0800

    first time

---
+++ sven-console/src/main/java/com/sven/console/main.java
@@ -0,0 +1,19 @@
+package com.sven.console;
+
+import com.sven.console.pojo.Person;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+public class main {
+    public static void main(String[] args){
+        KieServices kss = KieServices.get();
+        KieContainer kc = kss.getKieClasspathContainer();
+        KieSession ks = kc.newKieSession("testFirstRule");
+        Person p = new Person();
+        ks.insert(p);
+        int count = ks.fireAllRules();
+        System.out.println("执行了" + count + "规则");
+        ks.dispose();
+    }
+}
