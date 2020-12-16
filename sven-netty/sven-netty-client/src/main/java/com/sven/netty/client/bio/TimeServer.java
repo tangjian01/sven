69a94dec0895de05b4f81e6b45327f044d4dc692
tangjian01 <897863952@qq.com>
2020-12-16 19:22:14 +0800

    项目提交

---
+++ sven-netty/sven-netty-client/src/main/java/com/sven/netty/client/bio/TimeServer.java
@@ -0,0 +1,15 @@
+package com.sven.netty.client.bio;
+
+/**
+ * author: jian.tang@dmall.com
+ * date: 2020/12/13
+ * version: 2.0
+ *
+ */
+public class TimeServer {
+
+    public static void main(String[] args) {
+        int port = 8080;
+        
+    }
+}
