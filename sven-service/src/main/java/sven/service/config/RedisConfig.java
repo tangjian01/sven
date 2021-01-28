3b268570a7efa307cd0e479fc412b458e3c85b76
jian.tang <jian.tang@dmall.com>
2021-01-28 20:34:09 +0800

    集成redis

--- sven-service/src/main/java/sven/service/config/RedisConfig.java
+++ sven-service/src/main/java/sven/service/config/RedisConfig.java
@@ -1,13 +1,28 @@
 package sven.service.config;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.cache.RedisCacheWriter;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author ：897863952@qq.com
@@ -20,10 +35,63 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 @AutoConfigureAfter(RedisAutoConfiguration.class)
 //@EnableCaching
 public class RedisConfig {
+    private Integer defaultExp = 60;
     @Bean
-    public RedisTemplate<String, String> redisTemplate(JedisConnectionFactory connectionFactory) {
-        RedisTemplate<String, String> template = new StringRedisTemplate();
+    public RedisTemplate<String, Object> redisTemplate(JedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate();
         template.setConnectionFactory(connectionFactory);
+        //使用Jackson2JsonRedisSerializer来序列化和反序列化redis的value值
+        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        mapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(mapper);
+        template.setValueSerializer(jackson2JsonRedisSerializer);
+
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+        //使用StringRedisSerializer来序列化和反序列化redis的key值
+        template.setKeySerializer(stringRedisSerializer);
+        template.setKeySerializer(stringRedisSerializer);
+        // hash的key也采用String的序列化方式
+        template.setHashKeySerializer(stringRedisSerializer);
+        // value序列化方式采用jackson
+        template.setValueSerializer(jackson2JsonRedisSerializer);
+        // hash的value序列化方式采用jackson
+        template.setHashValueSerializer(jackson2JsonRedisSerializer);
+        template.afterPropertiesSet();
         return template;
     }
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory connectionFactory)
+    {
+        RedisCacheConfiguration configuration = this.defaultCacheConfig(defaultExp);
+        return new RedisCacheManager(
+                RedisCacheWriter.nonLockingRedisCacheWriter(connectionFactory),
+                // 默认策略，未配置的 key 会使用这个
+                configuration,
+                // 指定 key 策略
+                this.getRedisCacheConfigurationMap()
+        );
+    }
+    private Map<String, RedisCacheConfiguration> getRedisCacheConfigurationMap() {
+        Map<String, RedisCacheConfiguration> map = new HashMap<>();
+        // 设置缓存时间
+        map.put("t", this.defaultCacheConfig(1));
+        return map;
+    }
+    private RedisCacheConfiguration defaultCacheConfig(Integer second) {
+        Jackson2JsonRedisSerializer<Object> serializer = new Jackson2JsonRedisSerializer<>(Object.class);
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setVisibility(PropertyAccessor.ALL,JsonAutoDetect.Visibility.ANY);
+        mapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        serializer.setObjectMapper(mapper);
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofSeconds(second))
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(stringRedisSerializer))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(serializer));
+        return config;
+    }
+
 }
--- sven-web/src/test/java/sven/ws/web/SvenWebApplicationTests.java
+++ sven-web/src/test/java/sven/ws/web/SvenWebApplicationTests.java
@@ -1,11 +1,15 @@
 package sven.ws.web;
 
+import com.alibaba.fastjson.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import sven.service.config.StringRedisUtil;
+import sven.common.model.User;
+import sven.service.config.RedisUtil;
 import sven.service.iser.IUserService;
+
+import java.util.List;
 
 @SpringBootTest
 @ActiveProfiles("dev")
@@ -13,11 +17,17 @@ class SvenWebApplicationTests {
     @Autowired
     private IUserService userService;
     @Autowired
-    private StringRedisUtil stringRedisUtil;
+    private RedisUtil redisUtil;
     @Test
     void contextLoads() {
-        userService.list();
-        stringRedisUtil.set("t","1");
+        List<User> list = userService.list();
+        redisUtil.set("t","hello world");
+        redisUtil.set("t2",list);
+        Object t = redisUtil.get("t");
+        Object t2 = redisUtil.get("t2");
+
+        System.out.println(t);
+        System.out.println(JSONObject.toJSONString(t2));
     }
 
 }
